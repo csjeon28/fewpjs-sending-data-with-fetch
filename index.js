daf1963b0e6ab6280a7eaa680852fc22c31b4ed9
Christine Jeon <csjeon28@gmail.com>
2021-04-24 01:17:28 -0400

    commit tree 28efc49e872a83855bff2da262575fb213ac24ec

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(object => {
+        console.log(object)
+      document.body.innerHTML = object["id"]
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message
+    })
+}
+submitData();
+
+// const submitData = async (name, email) => {
+//     const response = await fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: "",
+//             email: ""
+//         })
+//     });
+//     const object = await response.json();
+//     console.log(object);
+// }
